524695645d7bd8f8f5f1617cfce1261890e3d03d
Perkles <henrryjk@hotmail.com>
2023-02-27 20:02:13 -0300

    Initial commit

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,32 @@
+package intro;
+
+public class Medidas {
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro codigo em java");
+
+
+        // Calculo da area - piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 4;
+        comprimento = 3;
+        resultado = largura * comprimento;
+
+        System.out.println("A area sera de " + resultado + " m²");
+
+    }
+        // Calculo da area reduzido
+
+    public void calculatorAreaCompacta(){
+        int largura = 5;
+        int comprimento = 6;
+        System.out.println("A area sera de "+ largura * comprimento +"m²");
+    }
+}
+
+
+
+
+
